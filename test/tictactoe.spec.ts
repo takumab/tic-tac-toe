19fc874332bed93fda774d47bb1e87862a04a133
takumab <takumab@gmail.com>
2024-08-08 23:15:11 +0100

    T, X is winner and O is winner of the first horizontal row

--- test/tictactoe.spec.ts
+++ test/tictactoe.spec.ts
@@ -1,6 +1,28 @@
 class Game {
+  private currentPlayer = "X";
+  private squares: string[] = [];
   getCurrentPlayer() {
-    return "X";
+    return this.currentPlayer;
+  }
+
+  play(square: number) {
+    this.squares[square] = this.currentPlayer;
+    if (this.currentPlayer == "X") {
+      this.currentPlayer = "O";
+    } else {
+      this.currentPlayer = "X";
+    }
+  }
+
+  getWinner() {
+    if (
+      this.squares[0] === "X" &&
+      this.squares[1] === "X" &&
+      this.squares[2] === "X"
+    ) {
+      return "X";
+    }
+    return "O";
   }
 }
 
@@ -11,5 +33,41 @@ describe("Tic Tac Toe Should", () => {
     let result = game.getCurrentPlayer();
 
     expect(result).toBe("X");
+  });
+
+  test("make 'O' the second player", () => {
+    let game = new Game();
+
+    game.play(1);
+    let result = game.getCurrentPlayer();
+
+    expect(result).toBe("O");
+  });
+
+  test("make X winner with 3 marks in first horizontal row", () => {
+    let game = new Game();
+
+    game.play(0);
+    game.play(3);
+    game.play(1);
+    game.play(5);
+    game.play(2);
+    const winner = game.getWinner();
+
+    expect(winner).toBe("X");
+  });
+
+  test("make O winner with 3 marks in first horizontal row", () => {
+    let game = new Game();
+
+    game.play(3);
+    game.play(0);
+    game.play(5);
+    game.play(1);
+    game.play(6);
+    game.play(2);
+    const winner = game.getWinner();
+
+    expect(winner).toBe("O");
   });
 });
