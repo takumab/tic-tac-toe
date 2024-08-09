103f7064995542a816e8a42e43b443f64fdd5516
takumab <takumab@gmail.com>
2024-08-09 22:39:49 +0100

    T, make 'X', winner 3 marks in second horizontal row

--- test/tictactoe.spec.ts
+++ test/tictactoe.spec.ts
@@ -1,6 +1,7 @@
 class Game {
   private currentPlayer = "X";
   private squares: string[] = [];
+
   getCurrentPlayer() {
     return this.currentPlayer;
   }
@@ -20,6 +21,13 @@ class Game {
       this.squares[1] === this.squares[2]
     ) {
       return this.squares[0];
+    }
+
+    if (
+      this.squares[3] === this.squares[4] &&
+      this.squares[4] === this.squares[5]
+    ) {
+      return this.squares[3];
     }
   }
 }
@@ -67,5 +75,18 @@ describe("Tic Tac Toe Should", () => {
     const winner = game.getWinner();
 
     expect(winner).toBe("O");
+  });
+
+  test("make X winner with 3 marks in second horizontal row", () => {
+    let game = new Game();
+
+    game.play(3);
+    game.play(1);
+    game.play(4);
+    game.play(2);
+    game.play(5);
+    const winner = game.getWinner();
+
+    expect(winner).toBe("X");
   });
 });
