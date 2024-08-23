603b889c9a32b443a0ba61415ad1a8528edbd344
tacuma.bellford <tacuma.bellford@lego.com>
2024-08-23 17:29:28 +0100

    R

---
+++ src/tictactoe.ts
@@ -0,0 +1,56 @@
+export class Game {
+  private currentPlayer = "X";
+  private squares: string[] = [];
+  private arrayOfWinningLines: number[][] = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  getCurrentPlayer() {
+    return this.currentPlayer;
+  }
+
+  play(square: number) {
+    if (this.squares[square] && this.squares[square] !== this.currentPlayer) {
+      throw new Error(
+        `Can't play on an already played squared: ${this.currentPlayer}`,
+      );
+    }
+    this.squares[square] = this.currentPlayer;
+    this.switchPlayer();
+  }
+
+  getWinner() {
+    return this.findWinner();
+  }
+
+  private findWinner() {
+    for (
+      let winningLineIndex = 0;
+      winningLineIndex < this.arrayOfWinningLines.length;
+      winningLineIndex++
+    ) {
+      const winningLine = this.arrayOfWinningLines[winningLineIndex];
+      if (
+        this.squares[winningLine[0]] === this.squares[winningLine[1]] &&
+        this.squares[winningLine[1]] === this.squares[winningLine[2]]
+      ) {
+        return this.squares[winningLine[0]];
+      }
+    }
+  }
+
+  private switchPlayer() {
+    if (this.currentPlayer == "X") {
+      this.currentPlayer = "O";
+    } else {
+      this.currentPlayer = "X";
+    }
+  }
+}
